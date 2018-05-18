a8bef1f8758daf9d9d0953f9e675d9a058af2dba
Sindre Wilting <sindrewilting@gmail.com>
2018-05-18 19:04:08 +0200

    Added a Concepts project

--- Source/Admin/Domain/Project/CreateProject.cs
+++ Source/Admin/Domain/Project/CreateProject.cs
@@ -11,7 +11,7 @@ namespace Domain.Project
 {
     public class CreateProject : ICommand
     {
-        public Guid Id { get; set; }
+        public Guid ProjectId { get; set; }
 
         public string Name { get; set; }
 
